445ea7b81f61b585c9710ea9f4b77cffc96fca01
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-07-15 12:52:36 +0530

    Update main.py

--- ForceSubscribeBot/main.py
+++ ForceSubscribeBot/main.py
@@ -42,7 +42,7 @@ async def main(bot: Client, msg: Message):
                 elif action == 'warn':
                     await msg.reply(f"ʜᴇʏ ᴅᴇᴀʀ  {msg.from_user.mention},\n\nʏᴏᴜ ᴍᴜsᴛ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ {mention} ᴛᴏ ᴄʜᴀᴛ ʜᴇʀᴇ ᴛʜᴇɴ ᴘʀᴇss ᴏɴ ᴜɴᴍᴜᴛᴇ ʙᴜᴛᴛᴏɴ.",
                     disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✨ sᴜʙsᴄʀɪʙᴇ  ᴍʏ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]))
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✨Nᴏᴛᴇs ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]))
                     await msg.delete()
                     return
 
@@ -52,7 +52,7 @@ async def main(bot: Client, msg: Message):
                     await msg.reply("ʙᴀɴɴᴇᴅ ᴍᴇᴍʙᴇʀ ʙᴇᴄᴀᴜsᴇ  ɴᴏᴛ  ᴊᴏɪɴᴇᴅ ғᴏʀᴄᴇ sᴜʙsʀɪʙᴇ ᴄʜᴀᴛ")
                     await msg.delete()
                     return
-                buttons = [[InlineKeyboardButton("✨ sᴜʙsᴄʀɪʙᴇ  ᴍʏ ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]
+                buttons = [[InlineKeyboardButton("✨ Nᴏᴛᴇs ᴄʜᴀɴɴᴇʟ  ✨", url=link)]]
                 if action == 'mute': 
                     await msg.chat.restrict_member(user_id, ChatPermissions(can_send_messages=False))
                     buttons.append([InlineKeyboardButton("ᴜɴᴍᴜᴛᴇ ᴍᴇ", callback_data=f"joined+{msg.from_user.id}")])
